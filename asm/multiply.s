838ba8e53d95d3d197a7ba087be48457da46b1a4
Brooke Chalmers <breq@breq.dev>
2022-05-16 00:11:34 -0400

    keep the result in the accumulator

--- asm/multiply.s
+++ asm/multiply.s
@@ -1,6 +1,5 @@
 FIRST = $00
 SECOND = $01
-RESULT = $02
 
   .org $8000
 reset:
@@ -8,25 +7,21 @@ reset:
   STA FIRST
   LDA #$09; Second factor (4 bits)
   STA SECOND
-  LDA #$00; Initialize the result to 0
-  STA RESULT
+  LDA #$00; Result (8 bits)
   LDX #$04; X register used to count the iteration
 
 .loop:
   LSR FIRST; Shifts the first factor to the right, putting the LSB in the carry flag so we can test it
   BCC .no_add
 
-  CLC; Clear the carry flag (since we know it's set)
-  LDA RESULT
+  CLC ; Clear the carry flag (since we know its set)
   ADC SECOND; Add the second factor to the result
-  STA RESULT
 
 .no_add:
   ASL SECOND
   DEX
   BNE .loop; While X is not 0
 
-  LDA RESULT
   STA $4000
 
 end:
