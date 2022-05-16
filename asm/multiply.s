c4bb3725f7f31a6ec553c2d944c7146419079461
Brooke Chalmers <breq@breq.dev>
2022-05-16 00:17:09 -0400

    don't use an explicit iteration count

--- asm/multiply.s
+++ asm/multiply.s
@@ -3,12 +3,11 @@ SECOND = $01
 
   .org $8000
 reset:
-  LDA #$0D; First factor (4 bits)
+  LDA #$09; First factor (4 bits)
   STA FIRST
-  LDA #$09; Second factor (4 bits)
+  LDA #$05; Second factor (4 bits)
   STA SECOND
   LDA #$00; Result (8 bits)
-  LDX #$04; X register used to count the iteration
 
 .loop:
   LSR FIRST; Shifts the first factor to the right, putting the LSB in the carry flag so we can test it
@@ -19,7 +18,6 @@ reset:
 
 .no_add:
   ASL SECOND
-  DEX
   BNE .loop; While X is not 0
 
   STA $4000
