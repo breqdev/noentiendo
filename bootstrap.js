852c677682231c87f60d54a51a2b51d5e69e07df
Brooke Chalmers <breq@breq.dev>
2022-09-12 16:09:14 -0400

    update path?

--- bootstrap.js
+++ bootstrap.js
@@ -1188,7 +1188,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "/noentiendo/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
