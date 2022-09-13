04b6b7c21e390ccf55aa3564235bca8090c44d91
Brooke Chalmers <breq@breq.dev>
2022-09-13 15:32:58 -0400

    web: improvements to canvas focus

--- www/index.js
+++ www/index.js
@@ -4,6 +4,10 @@ import charBin from "../pet/char.bin";
 import editorBin from "../pet/editor.bin";
 import kernalBin from "../pet/kernal.bin";
 
+console.log("hiii");
+console.log("focusing canvas");
+document.getElementById("canvas").focus();
+
 let [basic, char, editor, kernal] = [basicBin, charBin, editorBin, kernalBin]
   .map((x) => x.split(";")[1].split(",")[1])
   .map((x) => Uint8Array.from(atob(x), (c) => c.charCodeAt(0)));
