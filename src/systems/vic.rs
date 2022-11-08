d80c76898b39619b971f2ec0383eb31160f64754
Brooke Chalmers <breq@breq.dev>
2022-11-07 22:34:00 -0500

    try different types of memory?

--- src/systems/vic.rs
+++ src/systems/vic.rs
@@ -1,10 +1,10 @@
 use crate::memory::{
-  ActiveInterrupt, BlockMemory, BranchMemory, Memory, NullMemory, RomFile, SystemInfo,
+  ActiveInterrupt, BlockMemory, BranchMemory, Memory, RomFile, SharedMemory, SystemInfo, CallbackMemory,
 };
-use crate::platform::PlatformProvider;
+use crate::platform::{Color, PlatformProvider, WindowConfig};
 use crate::system::System;
 use crate::systems::SystemFactory;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 pub struct Vic20SystemRoms {
   pub character: RomFile,
@@ -45,6 +45,11 @@ impl VicChipSpeaker {
     self.on = (value & 0x80) != 0;
     self.note = value & 0x7f;
   }
+
+  fn reset(&mut self) {
+    self.on = false;
+    self.note = 0;
+  }
 }
 
 struct VicChipLightPen {
@@ -72,10 +77,24 @@ impl VicChipLightPen {
   fn write_y(&mut self, value: u8) {
     self.y = value;
   }
+
+  fn reset(&mut self) {
+    self.x = 0;
+    self.y = 0;
+  }
 }
 
 // Source: http://tinyvga.com/6561
 struct VicChip {
+  // Associated Memory
+  platform: Arc<dyn PlatformProvider>,
+  vram: SharedMemory,
+  vram_vec: Vec<u8>,
+  characters: SharedMemory,
+  colors: SharedMemory,
+
+  // Registers
+
   // TV scan settings
   scan_mode: bool,
 
@@ -120,8 +139,22 @@ struct VicChip {
 }
 
 impl VicChip {
-  fn new() -> Self {
+  fn new(platform: Arc<dyn PlatformProvider>, characters: RomFile) -> Self {
+    platform.request_window(WindowConfig::new(23 * 8, 22 * 8, 2.0));
+
+    let vram = CallbackMemory::new(
+      Box::new(|address| {
+        self.vram.read(address)
+      })
+    )
+
     Self {
+      platform,
+      vram: SharedMemory::new(Box::new()),
+      vram_vec: vec![0; 0x0200],
+      characters: SharedMemory::new(Box::new(BlockMemory::from_file(0x1000, characters))),
+      colors: SharedMemory::new(Box::new(BlockMemory::ram(0x0200))),
+
       scan_mode: false,
       left_draw_offset: 12,
       top_draw_offset: 38,
@@ -145,6 +178,18 @@ impl VicChip {
       background_color: 1,
       character_table_values: 0,
     }
+  }
+
+  fn vram(&self) -> Box<dyn Memory> {
+    Box::new(self.vram.clone())
+  }
+
+  fn characters(&self) -> Box<dyn Memory> {
+    Box::new(self.characters.clone())
+  }
+
+  fn colors(&self) -> Box<dyn Memory> {
+    Box::new(self.colors.clone())
   }
 }
 
@@ -218,7 +263,28 @@ impl Memory for VicChip {
   }
 
   fn reset(&mut self) {
-    *self = Self::new();
+    self.scan_mode = false;
+    self.left_draw_offset = 12;
+    self.top_draw_offset = 38;
+    self.column_count = 22;
+    self.vram_line_9 = true;
+    self.raster_counter = 0;
+    self.row_count = 23;
+    self.double_size_chars = false;
+    self.vram_address_top = 15;
+    self.light_pen.reset();
+    self.potentiometer_1 = 0xFF;
+    self.potentiometer_2 = 0xFF;
+    self.speaker_alto.reset();
+    self.speaker_tenor.reset();
+    self.speaker_soprano.reset();
+    self.speaker_noise.reset();
+    self.speaker_volume = 0;
+    self.aux_color = 0;
+    self.border_color = 3;
+    self.reverse_field = true;
+    self.background_color = 1;
+    self.character_table_values = 0;
   }
 
   fn poll(&mut self, _info: &SystemInfo) -> ActiveInterrupt {
@@ -231,25 +297,28 @@ pub struct Vic20SystemFactory {}
 impl SystemFactory<Vic20SystemRoms> for Vic20SystemFactory {
   fn create(roms: Vic20SystemRoms, platform: Arc<dyn PlatformProvider>) -> System {
     let low_ram = BlockMemory::ram(0x0400);
-    let main_ram = BlockMemory::ram(0x1000);
-    let expansion_ram = NullMemory::new();
+    let main_ram = BlockMemory::ram(0x0E00);
 
-    let characters = BlockMemory::from_file(0x1000, roms.character);
-
-    let io = VicChip::new();
+    let vic_chip = VicChip::new(platform, roms.character);
 
     let basic_rom = BlockMemory::from_file(0x2000, roms.basic);
     let kernel_rom = BlockMemory::from_file(0x2000, roms.kernal);
 
+    let vram = vic_chip.vram();
+    let characters = vic_chip.characters();
+    let colors = vic_chip.colors();
+
     let memory = BranchMemory::new()
       .map(0x0000, Box::new(low_ram))
       .map(0x1000, Box::new(main_ram))
-      .map(0x2000, Box::new(expansion_ram))
-      .map(0x8000, Box::new(characters))
-      .map(0x9000, Box::new(io))
+      .map(0x1E00, vram)
+      // .map(0x2000, Box::new(expansion_ram))
+      .map(0x8000, characters)
+      .map(0x9000, Box::new(vic_chip))
+      .map(0x9600, colors)
       .map(0xC000, Box::new(basic_rom))
       .map(0xE000, Box::new(kernel_rom));
 
-    System::new(Box::new(memory), 1_000)
+    System::new(Box::new(memory), 1_000_000)
   }
 }
