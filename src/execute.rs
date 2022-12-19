a71ab5bdc6171ff03f3fafab17dcb30a461c4ed1
Dillon Scott <dillonscott.2024@gmail.com>
2022-12-18 20:45:18 -0500

    rla

--- src/execute.rs
+++ src/execute.rs
@@ -453,7 +453,7 @@ impl Execute for System {
 
       0x03 | 0x07 | 0x0F | 0x13 | 0x17 | 0x1B | 0x1F => {
         // SLO
-        let (address, cycles) = self.fetch_operand_address(opcode);
+        let (address, address_cycles) = self.fetch_operand_address(opcode);
         let value = self.read(address);
         let result = value << 1;
 
@@ -461,12 +461,31 @@ impl Execute for System {
         self.registers.sr.set_nz(result);
         self.write(address, result);
 
+        let (value, value_cycles) = self.fetch_operand_value(opcode);
         self.registers.a |= value;
         self.registers.sr.set_nz(self.registers.a);
-        Ok(cycles + 2)
+
+        Ok(address_cycles + value_cycles + 2)
       }
 
-      // TODO: RLA, SRE, RRA, SAX, LAX, DCP, ISC, ANC, ALR, ARR, XAA, AXS, SBC, SHY, AHX, TAS
+      0x23 | 0x27 | 0x2F | 0x33 | 0x37 | 0x3B | 0x3F => {
+        // RLA
+        let (address, address_cycles) = self.fetch_operand_address(opcode);
+        let value = self.read(address);
+        let result = (value << 1) | (self.registers.sr.read(flags::CARRY) as u8);
+
+        self.registers.sr.write(flags::CARRY, value & 0x80 != 0);
+        self.registers.sr.set_nz(result);
+        self.write(address, result);
+
+        let (value, value_cycles) = self.fetch_operand_value(opcode);
+        self.registers.a &= value;
+        self.registers.sr.set_nz(self.registers.a);
+
+        Ok(address_cycles + value_cycles + 2)
+      }
+
+      // TODO: SRE, RRA, SAX, LAX, DCP, ISC, ANC, ALR, ARR, XAA, AXS, SBC, SHY, AHX, TAS
       _ => {
         println!("Unimplemented opcode: {:02X}", opcode);
         Err(())
