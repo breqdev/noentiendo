bf2e783d086229e6fafff2152cf86e621273c6d4
Brooke Chalmers <breq@breq.dev>
2023-11-15 23:36:57 -0500

    Improvements to LoggingMemory

--- src/memory/logging.rs
+++ src/memory/logging.rs
@@ -1,32 +1,50 @@
 use super::{ActiveInterrupt, Memory, SystemInfo};
 
-pub struct LoggingMemory(pub Box<dyn Memory>);
+pub struct LoggingMemory {
+  backing: Box<dyn Memory>,
+  message: String,
+  offset: u16,
+}
+
+impl LoggingMemory {
+  pub fn new(backing: Box<dyn Memory>, message: &str, offset: u16) -> LoggingMemory {
+    LoggingMemory {
+      backing,
+      message: message.to_owned(),
+      offset,
+    }
+  }
+}
 
 impl Memory for LoggingMemory {
   fn read(&mut self, address: u16) -> u8 {
-    let value = self.0.read(address);
+    let value = self.backing.read(address);
     println!(
-      "[Memory Read]: address {:04X}, value {:02X}",
-      address, value
-    );
+      "[Memory Read]: {} address {:04X}, value {:02X}",
+      self.message,
+      address + self.offset,
+      value
+    }
     value
   }
 
   fn write(&mut self, address: u16, value: u8) {
-    self.0.write(address, value);
+    self.backing.write(address, value);
     println!(
-      "[Memory Write]: address {:04X}, value {:02X}",
-      address, value
+      "[Memory Write]: {} address {:04X}, value {:02X}",
+      self.message,
+      address + self.offset,
+      value
     );
   }
 
   fn reset(&mut self) {
-    self.0.reset();
-    println!("[Memory Reset]");
+    self.backing.reset();
+    println!("[Memory Reset]: {}", self.message);
   }
 
   fn poll(&mut self, cycles: u32, info: &SystemInfo) -> ActiveInterrupt {
-    println!("[Memory Poll]");
-    self.0.poll(cycles, info)
+    // println!("[Memory Poll]: {}", self.message);
+    self.backing.poll(cycles, info)
   }
 }
